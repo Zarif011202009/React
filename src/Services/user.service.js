f84adf48d5111a6338593b27ff9a6f398de45f90
Zarif011202009 <szarif202009@bscse.uiu.ac.bd>
2023-04-29 09:31:30 +0600

    Commit by Zarif

--- src/Services/user.service.js
+++ src/Services/user.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://192.168.0.102:3000";
+const BASE_URL = "http://10.10.245.211:3000";
 
 export const addUser = (data) =>{
     console.log(data, `${BASE_URL}/user`)
